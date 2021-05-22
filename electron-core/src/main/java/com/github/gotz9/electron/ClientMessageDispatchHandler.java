80b08ae0d380fe50a5be802e600bdff13b651da3
xh-gotz9 <xh353127981@gmail.com>
2021-05-23 02:53:06 +0800

    优化 ClientMessageDispatchHandler 日志

--- electron-core/src/main/java/com/github/gotz9/electron/ClientMessageDispatchHandler.java
+++ electron-core/src/main/java/com/github/gotz9/electron/ClientMessageDispatchHandler.java
@@ -3,12 +3,18 @@ package com.github.gotz9.electron;
 import com.github.gotz9.electron.handler.IHandler;
 import com.github.gotz9.electron.handler.IHandlerManager;
 import com.github.gotz9.electron.protocol.message.ClientMessage;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 
 @ChannelHandler.Sharable
 public class ClientMessageDispatchHandler extends SimpleChannelInboundHandler<ClientMessage> {
+
+    private static final Logger logger = LoggerFactory.getLogger(ClientMessageDispatchHandler.class);
 
     private final IHandlerManager handlerManager;
 
@@ -20,14 +26,16 @@ public class ClientMessageDispatchHandler extends SimpleChannelInboundHandler<Cl
     protected void channelRead0(ChannelHandlerContext ctx, ClientMessage msg) throws Exception {
         IHandler handler = handlerManager.getHandler(msg.getType().getNumber());
 
-        if (handler == null)
+        if (handler == null) {
             // 无法处理的消息
+            logger.warn("unhandlable message: not found handler for {}", msg.getType());
             return;
+        }
 
         try {
             handler.handle(ctx.channel(), msg.getLogin());
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.error("handler exception", e);
         }
     }
 
